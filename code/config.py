3a0062c66c5df142b8acd975165ae916380c97c3
Ahmed Sabry <52863189+sabry2000@users.noreply.github.com>
2021-05-26 22:41:51 -0700

    updated for both mac and windows os

--- code/config.py
+++ code/config.py
@@ -7,11 +7,12 @@ __allPlayersTextFileName = "AllPlayers.txt"
 __allTeamsTextFileName = "AllTeams.txt"
 __allTeamsObjectFileName = "TeamsObject"
 __allNamesFileName = "names.json"
+seperator = os.path.sep
 
 __currentDirectory = os.path.dirname(__file__)
-__resultsFolder = os.path.join(__currentDirectory, '..\\', 'results\\')
+__resultsFolder = os.path.join(__currentDirectory, str('..' + seperator + seperator), str('results' + seperator + seperator))
 
 STR_AllPlayersInformationDirectory = os.path.join(__resultsFolder, __allPlayersTextFileName)
 STR_AllTeamsInformationDirectory = os.path.join(__resultsFolder, __allTeamsTextFileName)
 STR_AllTeamsObjectDirectory = os.path.join(__currentDirectory, __allTeamsObjectFileName)
-STR_AllNamesJSONDirectory = os.path.join(__currentDirectory,'..\\', __allNamesFileName)
+STR_AllNamesJSONDirectory = os.path.join(__currentDirectory, str('..' + seperator + seperator), __allNamesFileName)
